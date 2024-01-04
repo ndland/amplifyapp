23222a33eadb7411f70d9d24869a1ad214b774eb
Nick Land <land.d.nicholas@gmail.com>
2024-01-04 08:46:10 -0500

    finished module 4

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,4 +1,11 @@
 export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "amplifyapp": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
   "auth": {
     "amplifyapp04ca0361": {
       "AppClientID": "string",
